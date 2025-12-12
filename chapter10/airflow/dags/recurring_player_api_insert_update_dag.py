477da8ae1fdb3921fd39875e2949b098fcfabf24
Dawid Maleszka <85014412+dave-m91@users.noreply.github.com>
2025-12-12 11:50:46 +0000

    Utworzenie grafu DAG

---
+++ chapter10/airflow/dags/recurring_player_api_insert_update_dag.py
@@ -0,0 +1,51 @@
+import datetime
+import logging
+from airflow.decorators import dag
+from airflow.providers.http.operators.http import HttpOperator
+from airflow.operators.python import PythonOperator
+from shared_functions import upsert_player_data
+
+def health_check_response(response):
+    logging.info(f"Kod stanu odpowiedzi: {response.status_code}")
+    logging.info(f"Treść odpowiedzi: {response.text}")
+    return response.status_code == 200 and response.json() == {
+        "message": "Test stanu API zakończony sukcesem"
+    }
+
+def insert_update_player_data(**context):
+    player_json = context["ti"].xcom_pull(task_ids="api_player_query")
+
+    if player_json:
+        upsert_player_data(player_json)
+    else:
+        logging.warning("nie znaleziono zawodnika.")
+
+@dag(schedule_interval = None)
+def recurring_player_api_insert_update_dag():
+    api_health_check_task = HttpOperator(
+        task_id = "check_api_health_check_endpoint",
+        http_conn_id = "sportsworldcentral_api",
+        endpoint = "/",
+        method = "GET",
+        headers = {"Content-Type": "application/json"},
+        response_check = health_check_response
+    )
+    temp_min_last_changed_date = "2024-04-01"
+
+    api_player_query_task = HttpOperator(
+        task_id = "api_player_query",
+        http_conn_id = "sportworldscentral_url",
+        endpoint = (f"/v0/players/?skip/0&limit=100000&minimum_last_changed_date={temp_min_last_changed_date}"),
+        method = "GET",
+        headers = {"Content-Type": "application/json"}
+    )
+
+    player_sqlite_upsert_task = PythonOperator(
+        task_id = "player_sqlite_upsert",
+        python_callable = insert_update_player_data,
+        provide_context = True
+    )
+
+    api_health_check_task >> api_player_query_task >> player_sqlite_upsert_task
+
+dag_instance = recurring_player_api_insert_update_dag()
