d864937cb9fc86aaa040957d079f55b051cadeeb
Dawid Maleszka <85014412+dave-m91@users.noreply.github.com>
2025-12-16 09:47:29 +0000

    Dodanie tabeli team

--- chapter10/airflow/dags/shared_functions.py
+++ chapter10/airflow/dags/shared_functions.py
@@ -43,3 +43,38 @@ def upsert_player_data(player_json):
     else:
         logging.warning("Nie znaleziono danych zawodnika")
         raise ValueError("nie znaleziono danych gracza. Zadanie nie powiodło się z powodu braku dancyh")
+    
+
+def upsert_team_data(team_json):
+    import sqlite3
+    import pandas as pd
+    database_conn_id = "analytics_database"
+    connection = BaseHook.get_connection(database_conn_id)
+    sqlite_db_path = connection.schema
+    if team_json:
+        team_data = json.loads(team_json)
+        with sqlite3.connect(sqlite_db_path) as conn:
+            cursor = conn.cursor()
+            for team in team_data:
+                try:
+                    cursor.execute("""
+                                    INSERT INTO team (
+                                   league_id,
+                                   team_id,
+                                   team_name,
+                                   last_changed_date)
+                                   VALUES (?, ?, ?, ?)
+                                   ON CONFLICT(league_id) DO UPDATE
+                                   SET team_id excluded.team_id,
+                                   team_name excluded.team_name,
+                                   last_changed_date excluded.last_changed_date""",
+                                   (team["league_id"],
+                                    team["team_id"],
+                                    team["team_name"],
+                                    team["last_changed_date"]))
+                except Exception as e:
+                    logging.error(f"Nie udało się wstawić danych drużyny {team['team_id']}: {e}")
+                    raise
+    else:
+        logging.warning("Nie znaleziono danych drużyny")
+        raise ValueError("Nie znaleziono danych drużyny. Zadanie nie powiodło się z powodu braku danych")
